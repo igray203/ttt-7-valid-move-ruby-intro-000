c5ad9a38949d441cc990e519d55b40e8b48e21e0
ian gray <igrayscale@gmail.com>
2019-08-17 20:46:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,30 @@
 # code your #valid_move? method here
+def valid_move?(array,index)
+  if index.between?(0,8) == true
+    return nil
+#  elsif position_taken?(array,index) == true
+#    return true
+#  elsif index.between?(0,8) && position_taken?(array,index) == true
+#    return 'first'
+#  elsif index.between?(0,8) && position_taken?(array,index) == false
+#    return 'second'
+#  if index.between?(0,8) || position_taken?(array,index) == false
+#    return 'third'
+#  elsif index.between?(0,8) || position_taken?(array,index) == true
+#    return 'fourth'
 
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(array,index)
+  if array[index] == ""
+    return false
+  elsif array[index] == " "
+    return false
+  elsif array[index] == nil
+    return false
+  elsif array[index] == "X" || "O"
+    return true
+  end
+end
